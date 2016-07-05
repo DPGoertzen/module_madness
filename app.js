1d0c08c8577ca6584ef3ae5eff93a1a84dd7960e
DPGoertzen <dpgoertzen@gmail.com>
2016-07-05 17:52:15 -0500

    Working functionality.

---
+++ app.js
@@ -0,0 +1,3 @@
+var mod3 = require('./mod3.js');
+mod3.accountHeader();
+mod3.makeAccountBalance();
---
+++ mod3.js
@@ -0,0 +1,16 @@
+var mod1 = require('./mod1.js');
+var mod2 = require('./mod2.js');
+
+module.exports = {
+makeAccountBalance : makeAccountBalance,
+accountHeader : accountHeader,
+};
+
+
+function makeAccountBalance(){
+  console.log(mod2.toUSCurrency(mod1.randomNum(100, 100000)));
+}
+
+function accountHeader(){
+  console.log("Account balance:\n");
+}
